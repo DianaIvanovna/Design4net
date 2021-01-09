900bf403a55bce0bf8d6e8275287a64ccfbd50f2
Diana Garbuz <GDI.1999@yandex.ru>
2021-01-09 17:22:30 +0300

    header adaptive

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 import "./style.css";
-import './main.scss'
+import "./main.scss";
 
-const numbers = [2, 3, 5];
+(function () {
+  const header = document.querySelector('.header');
+  const headerBackground = document.querySelector('.background__mobile');
+  const headerMenu = document.querySelector('.header__menu');
+
+
+  headerMenu.addEventListener('click', openMobile);
+  headerBackground.addEventListener('click',closeMobile);
+
+  function openMobile(){ // открытие мобильной версии header
+    header.classList.toggle('header_open-mobile');
+    headerBackground.classList.toggle('background__mobile_active');
+  };
+  function closeMobile(){ // закрытие мобильной версии header
+    header.classList.remove('header_open-mobile');
+    headerBackground.classList.remove('background__mobile_active');
+  };
+
+})();
+
