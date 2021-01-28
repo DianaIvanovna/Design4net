8810bbe1f3f3d906f0a78eba42396d73469cdf15
Diana Garbuz <GDI.1999@yandex.ru>
2021-01-28 04:11:54 +0300

    анимация в портфолио

--- src/pages/mobileye/mobileye.js
+++ src/pages/mobileye/mobileye.js
@@ -1,2 +1,14 @@
 import "./mobileye.css";
 import "./mobileye.scss";
+import imgSite from "../../images/mobileye/mobileye-pictures.jpg";
+import Header from '../../js/components/Header';
+
+(function () {
+  const header = new Header();
+
+  const devices = document.querySelectorAll(".portfolio__site");
+  devices.forEach((item)=>{
+    item.style.backgroundImage = `url("${imgSite}")`;
+  })
+
+})();
--- src/pages/runovo/runovo.js
+++ src/pages/runovo/runovo.js
@@ -1,2 +1,15 @@
 import "./runovo.css";
 import "./runovo.scss";
+
+import imgSite from "../../images/runovo/runovo-pictures.jpg";
+import Header from '../../js/components/Header';
+
+(function () {
+  const header = new Header();
+
+  const devices = document.querySelectorAll(".portfolio__site");
+  devices.forEach((item)=>{
+    item.style.backgroundImage = `url("${imgSite}")`;
+  })
+
+})();
